0eaf311c2b1b2f5aff1a334e6c8be9c75e7b4ecc
ap21001 <ap21001@yuge.kosen-ac.jp>
2021-10-08 14:19:01 +0900

    Create nbgrader_config.py

---
+++ nbgrader_config.py
@@ -0,0 +1,30 @@
+c = get_config()
+#c.CourseDirectory.root = '/home/admin/notebook/nbtestadmin'
+###############################################################################
+# Begin additions by nbgrader quickstart
+###############################################################################
+
+# You only need this if you are running nbgrader on a shared
+# server set up.
+#c.CourseDirectory.course_id = "nbtestadmin"
+#c.Exchange.root = "/tmp/exchange"
+# server set up.
+#c.NbGrader.logfile = "/home/ap21001/log/nbgrader.log"
+#c.CourseDirectory.course_id = "nbtestadmin"
+#c.Exchange.course_id = "nbtestadmin"
+#c.Exchange.root = "/tmp/exchange"
+#c.Exchange.course_id = "nbtestadmin"
+#c.NbGrader.logfile = '.nbgrader.log'
+
+#c = get_config()
+#c.CourseDirectory.course_id = "nbtestadmin"
+#c.Exchange.root = '/tmp/exchange'
+#c.Exchange.timestamp_format = '%Y-%m-%d %H:%M:%S %Z'
+#c.Exchange.timezone = 'JST'
+#c.NbGrader.logfile = '.nbgrader.log'
+#c.NbGrader.logfile = '.nbgrader.log'
+c.CourseDirectory.course_id = "nbtestadmin"
+c.Exchange.root = "/tmp/exchange"
+c.NbGrader.logfile = '.nbgrader.log'
+
+
